5234a068d68472abab9724af43139b760cb70cb5
tison <wander4096@gmail.com>
2025-10-20 06:46:57 +0800

    chore: upgrade practices
    Signed-off-by: tison <wander4096@gmail.com>

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! An xtask binary for managing workspace tasks.
+
 use std::process::Command as StdCommand;
 
 use clap::Parser;
