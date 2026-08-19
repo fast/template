121820272fe52e64a413427717eb4733bedb5158
tison <wander4096@gmail.com>
2026-08-19 20:52:04 +0800

    chore: migrate HawkEye to v7

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -207,10 +207,10 @@ fn make_doc_cmd() -> StdCommand {
 }
 
 fn make_hawkeye_cmd(fix: bool) -> StdCommand {
-    ensure_installed("hawkeye", "hawkeye");
+    ensure_installed("hawkeye", "hawkeye@7.0.0");
     let mut cmd = find_command("hawkeye");
     if fix {
-        cmd.args(["format", "--fail-if-updated=false"]);
+        cmd.args(["format"]);
     } else {
         cmd.args(["check"]);
     }
